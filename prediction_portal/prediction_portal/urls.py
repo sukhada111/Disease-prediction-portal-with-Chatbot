b9b80307f9ddf61824f0af15249301e0bb8a59cf
unknown <rhutuja.t@somaiya.edu>
2021-11-03 02:09:24 +0530

    Added template

--- prediction_portal/prediction_portal/urls.py
+++ prediction_portal/prediction_portal/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+import home.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home.views.home,name='home')
 ]
