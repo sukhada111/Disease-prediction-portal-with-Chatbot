c6d12a22884be90bfb5492d73214054d21d9c427
sukhada111 <sukhada9987@gmail.com>
2022-02-28 23:33:50 +0530

    implemented sentiment analysis model

---
+++ ML Models/model_LSTM.py
@@ -0,0 +1,498 @@
+from time import time
+import random
+import pandas as pd
+import numpy as np
+import tensorflow
+config = tensorflow.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+
+
+
+df_raw=pd.read_csv('Dataset/training_tweets.csv',encoding = "ISO-8859-1", header=None)
+
+ # As the data has no column titles, we will add our own
+df_raw.columns = ["label", "time", "date", "query", "username", "text"]
+
+df = df_raw[['label', 'text']]
+df_pos = df[df['label'] == 4]
+df_neg = df[df['label'] == 0]
+print(len(df_pos), len(df_neg))
+
+df_pos = df_pos.iloc[:int(len(df_pos)/3)]
+df_neg = df_neg.iloc[:int(len(df_neg)/3)]
+print(len(df_pos), len(df_neg))
+
+# Concatenating both positive and negative groups and storing them back into a single dataframe
+df = pd.concat([df_pos, df_neg])
+len(df)
+
+from nltk.tokenize import TweetTokenizer
+
+start_time = time()
+
+
+# The reduce_len parameter will allow a maximum of 3 consecutive repeating characters, while trimming the rest
+# For example, it will tranform the word: 'Helloooooooooo' to: 'Hellooo'
+tk = TweetTokenizer(reduce_len=True)
+
+data = []
+
+# Separating our features (text) and our labels into two lists to smoothen our work
+X = df['text'].tolist()
+Y = df['label'].tolist()
+
+# Building our data list, that is a list of tuples, where each tuple is a pair of the tokenized text
+# and its corresponding label
+for x, y in zip(X, Y):
+    if y == 4:
+        data.append((tk.tokenize(x), 1))
+    else:
+        data.append((tk.tokenize(x), 0))
+        
+# Printing the CPU time and the first 5 elements of our 'data' list
+print('CPU Time:', time() - start_time)
+data[:5]
+
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+
+import re, string
+
+# Stopwords are frequently-used words (such as “the”, “a”, “an”, “in”) that do not hold any meaning useful to extract sentiment.
+# If it's your first time ever using nltk, you can download nltk's stopwords using: nltk.download('stopwords')
+from nltk.corpus import stopwords
+STOP_WORDS = stopwords.words('english')
+
+# Defining a handy function in order to load a given glove file
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r', encoding="utf8") as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+        
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
+
+word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('ML Models/glove.6B.50d.txt')
+
+from tensorflow.keras.models import Sequential, load_model, Model
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LSTM, Bidirectional
+from tensorflow.keras.layers import Embedding
+from sklearn.model_selection import train_test_split
+
+# Defining a function that will initialize and populate our embedding layer
+
+def pretrained_embedding_layer(word_to_vec_map, word_to_index, max_len):
+    vocab_len = len(word_to_index) + 1
+    emb_dim = word_to_vec_map["unk"].shape[0] #50
+    
+    emb_matrix = np.zeros((vocab_len, emb_dim))
+    
+    for word, idx in word_to_index.items():
+        emb_matrix[idx, :] = word_to_vec_map[word]
+        
+    embedding_layer = Embedding(vocab_len, emb_dim, trainable=False, input_shape=(max_len,))
+    embedding_layer.build((None,))
+    embedding_layer.set_weights([emb_matrix])
+    
+    return embedding_layer
+
+# Further data cleaning
+# A custom function defined in order to fine-tune the cleaning of the input text.
+# This function is being "upgraded" such that it performs a more thourough cleaning of the data
+# in order to better fit our words embedding layer
+def cleaned(token):
+    if token == 'u':
+        return 'you'
+    if token == 'r':
+        return 'are'
+    if token == 'some1':
+        return 'someone'
+    if token == 'yrs':
+        return 'years'
+    if token == 'hrs':
+        return 'hours'
+    if token == 'mins':
+        return 'minutes'
+    if token == 'secs':
+        return 'seconds'
+    if token == 'pls' or token == 'plz':
+        return 'please'
+    if token == '2morow' or token == '2moro' or token=='tmrw' or token=='tomorow':
+        return 'tomorrow'
+    if token == '2day':
+        return 'today'
+    if token == '4got' or token == '4gotten':
+        return 'forget'
+    if token in ['hahah', 'hahaha', 'hahahaha', 'hehehe', 'hahahah']:
+        return 'haha'
+    if token == "mother's":
+        return "mother"
+    if token == "mom's":
+        return "mom"
+    if token == "dad's":
+        return "dad"
+    if token == 'bday' or token == 'b-day':
+        return 'birthday'
+    if token in ["i'm", "don't", "can't", "couldn't", "aren't", "wouldn't", "isn't", "didn't", "hadn't",
+                 "doesn't", "won't", "haven't", "wasn't", "hasn't", "shouldn't", "ain't","weren't", "should've", "would've", "here's","where's"]:
+        return token.replace("'", "")
+    if token in ['lmao', 'lolz', 'rofl']:
+        return 'lol'
+    if token == '<3':
+        return 'love'
+    if token == 'thanx' or token == 'thnx':
+        return 'thanks'
+    if token == 'goood':
+        return 'good'
+    if token in ['amp', 'quot', 'lt', 'gt', '½25', '..', '. .', '. . .']:
+        return ''
+    #added
+    if token in ['awh', 'aw', 'awww']:
+        return 'aww'
+    return token
+
+
+# This function will be our all-in-one noise removal function
+def remove_noise(tweet_tokens):
+
+    cleaned_tokens = []
+
+    for token in tweet_tokens:
+        # Eliminating the token if it is a link
+        token = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', " ", token)
+        # Eliminating the token if it is a mention
+        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+        
+        cleaned_token = cleaned(token.lower())
+        
+        if cleaned_token == "idk":
+            cleaned_tokens.append('i')
+            cleaned_tokens.append('dont')
+            cleaned_tokens.append('know')
+            continue
+        if cleaned_token == "i'll":
+            cleaned_tokens.append('i')
+            cleaned_tokens.append('will')
+            continue
+        if cleaned_token == "you'll":
+            cleaned_tokens.append('you')
+            cleaned_tokens.append('will')
+            continue
+        if cleaned_token == "we'll":
+            cleaned_tokens.append('we')
+            cleaned_tokens.append('will')
+            continue
+        if cleaned_token == "it'll":
+            cleaned_tokens.append('it')
+            cleaned_tokens.append('will')
+            continue
+        #added
+        if cleaned_token == "they'll" or cleaned_token== "they'l":
+            cleaned_tokens.append('they')
+            cleaned_tokens.append('will')
+            continue
+        if cleaned_token == "he'll" or cleaned_token== "he'l":
+            cleaned_tokens.append('he')
+            cleaned_tokens.append('will')
+            continue
+        if cleaned_token == "she'll" or cleaned_token== "she'l":
+            cleaned_tokens.append('she')
+            cleaned_tokens.append('will')
+            continue
+        
+        if cleaned_token == "it's":
+            cleaned_tokens.append('it')
+            cleaned_tokens.append('is')
+            continue
+        if cleaned_token == "i've":
+            cleaned_tokens.append('i')
+            cleaned_tokens.append('have')
+            continue
+        if cleaned_token == "you've":
+            cleaned_tokens.append('you')
+            cleaned_tokens.append('have')
+            continue
+        if cleaned_token == "we've":
+            cleaned_tokens.append('we')
+            cleaned_tokens.append('have')
+            continue
+        if cleaned_token == "they've":
+            cleaned_tokens.append('they')
+            cleaned_tokens.append('have')
+            continue
+        if cleaned_token == "you're":
+            cleaned_tokens.append('you')
+            cleaned_tokens.append('are')
+            continue
+        if cleaned_token == "we're":
+            cleaned_tokens.append('we')
+            cleaned_tokens.append('are')
+            continue
+        if cleaned_token == "they're":
+            cleaned_tokens.append('they')
+            cleaned_tokens.append('are')
+            continue
+        if cleaned_token == "let's":
+            cleaned_tokens.append('let')
+            cleaned_tokens.append('us')
+            continue
+        if cleaned_token == "she's":
+            cleaned_tokens.append('she')
+            cleaned_tokens.append('is')
+            continue
+        if cleaned_token == "he's":
+            cleaned_tokens.append('he')
+            cleaned_tokens.append('is')
+            continue
+        if cleaned_token == "that's":
+            cleaned_tokens.append('that')
+            cleaned_tokens.append('is')
+            continue
+        if cleaned_token == "i'd":
+            cleaned_tokens.append('i')
+            cleaned_tokens.append('would')
+            continue
+        if cleaned_token == "you'd":
+            cleaned_tokens.append('you')
+            cleaned_tokens.append('would')
+            continue
+        if cleaned_token == "there's":
+            cleaned_tokens.append('there')
+            cleaned_tokens.append('is')
+            continue
+        if cleaned_token == "what's":
+            cleaned_tokens.append('what')
+            cleaned_tokens.append('is')
+            continue
+        if cleaned_token == "how's":
+            cleaned_tokens.append('how')
+            cleaned_tokens.append('is')
+            continue
+        if cleaned_token == "who's":
+            cleaned_tokens.append('who')
+            cleaned_tokens.append('is')
+            continue
+        if cleaned_token == "y'all" or cleaned_token == "ya'll":
+            cleaned_tokens.append('you')
+            cleaned_tokens.append('all')
+            continue
+        #added by me
+        if cleaned_token == "hadnt":
+            cleaned_tokens.append('had')
+            cleaned_tokens.append('not')
+            continue
+        if cleaned_token == "shouldnt":
+            cleaned_tokens.append('should')
+            cleaned_tokens.append('not')
+            continue
+        if cleaned_token == "werent":
+            cleaned_tokens.append('were')
+            cleaned_tokens.append('not')
+            continue
+        if cleaned_token == "shouldve":
+            cleaned_tokens.append('should')
+            cleaned_tokens.append('have')
+            continue
+        if cleaned_token == "wouldve":
+            cleaned_tokens.append('would')
+            cleaned_tokens.append('have')
+            continue
+         
+
+        if cleaned_token.strip() and cleaned_token not in string.punctuation: 
+            cleaned_tokens.append(cleaned_token)
+            
+    return cleaned_tokens
+
+# Prevewing the remove_noise() output
+print(remove_noise(data[0][0]))
+
+start_time = time()
+
+unks = []
+UNKS = []
+
+def cleared(word):
+    res = ""
+    prev = None
+    for char in word:
+        if char == prev: continue
+        prev = char
+        res += char
+    return res
+
+def sentence_to_indices(sentence_words, word_to_index, max_len, i):
+    global X, Y
+    sentence_indices = []
+    for j, w in enumerate(sentence_words):
+        try:
+            index = word_to_index[w]
+        except:
+            UNKS.append(w)
+            w = cleared(w)
+            try:
+                index = word_to_index[w]
+            except:
+                index = word_to_index['unk']
+                unks.append(w)
+        X[i, j] = index
+
+cleaned_tokens_list = []
+
+# Removing noise from all the data, using the newly defined function
+for tokens, label in data:
+    x = remove_noise(tokens)
+    if x:
+        cleaned_tokens_list.append((x, label))
+
+print('Removed Noise, CPU Time:', time() - start_time)
+start_time = time()
+
+list_len = [len(i) for i, j in cleaned_tokens_list]
+max_len = max(list_len)
+print('max_len:', max_len)
+
+
+X = np.zeros((len(cleaned_tokens_list), max_len))
+Y = np.zeros((len(cleaned_tokens_list), ))
+
+
+for i, tk_lb in enumerate(cleaned_tokens_list):
+    tokens, label = tk_lb
+    sentence_to_indices(tokens, word_to_index, max_len, i)
+    Y[i] = label
+    
+print('Data Prepared for model, CPU Time:', time() - start_time)
+
+
+print(X[:5])
+print(Y[:5])
+
+unk = word_to_index['unk']
+
+n_unk_words = 0
+
+for x in X:
+    for y in x:
+        if y == unk:
+            n_unk_words += 1
+
+n_unk_words
+
+
+tensorflow.keras.backend.clear_session()
+# gpu_devices = tensorflow.config.experimental.list_physical_devices("GPU")
+# for device in gpu_devices:
+#     tensorflow.config.experimental.set_memory_growth(device, True)
+# gpus = tensorflow.config.list_physical_devices('GPU')
+# if gpus:
+#   try:
+#     # Currently, memory growth needs to be the same across GPUs
+#     for gpu in gpus:
+#       tensorflow.config.experimental.set_memory_growth(gpu, True)
+#     logical_gpus = tensorflow.config.list_logical_devices('GPU')
+#     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+#   except RuntimeError as e:
+#     # Memory growth must be set before GPUs have been initialized
+#     print(e)
+
+model_clean_data = Sequential()
+
+model_clean_data.add(pretrained_embedding_layer(word_to_vec_map, word_to_index, max_len))
+model_clean_data.add(Bidirectional(LSTM(units=128, return_sequences=True)))
+model_clean_data.add(Bidirectional(LSTM(units=128, return_sequences=False)))
+model_clean_data.add(Dense(units=1, activation='sigmoid'))
+
+model_clean_data.summary()
+
+model_clean_data.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0, stratify=Y)
+print("splitted")
+
+print(len(X_train))
+print(len(X_test))
+
+model_clean_data.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs = 5, batch_size = 512, shuffle=True)
+
+model_clean_data.save("BiLSTM_final-2.h5")
+
+history = model_clean_data.history
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+import matplotlib.pyplot as plt
+plt.plot(epochs, acc, 'bo', label = 'Training Accuracy')
+plt.plot(epochs, val_acc, 'r', label = 'Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+y_arrow = max(val_acc)
+x_arrow = val_acc.index(y_arrow) + 1
+plt.annotate(str(y_arrow)[:6],
+             (x_arrow, y_arrow),
+             xytext=(x_arrow + 5, y_arrow + .02),
+             arrowprops=dict(facecolor='orange', shrink=0.05))
+plt.xticks(epochs)
+
+plt.figure()
+plt.plot(epochs, loss, 'bo', label = 'Training Loss')
+plt.plot(epochs, val_loss, 'r', label = 'Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.xticks(epochs)
+plt.show()
+
+
+def sentence_to_indices(sentence_words, max_len):
+    X = np.zeros((max_len))
+    sentence_indices = []
+    for j, w in enumerate(sentence_words):
+        try:
+            index = word_to_index[w]
+        except:
+            w = cleared(w)
+            try:
+                index = word_to_index[w]
+            except:
+                index = word_to_index['unk']
+        X[j] = index
+    return X
+
+def predict_custom_tweet_sentiment(custom_tweet):
+    # Convert the tweet such that it can be fed to the model
+    x_input = sentence_to_indices(remove_noise(tk.tokenize(custom_tweet)), max_len)
+    
+    # Retrun the model's prediction
+    return model_clean_data.predict(np.array([x_input])).item()
+
+
+print(predict_custom_tweet_sentiment("I'm not happy you're here"))
+
+print(predict_custom_tweet_sentiment("I'm glad you're here!"))
+
+print(predict_custom_tweet_sentiment("I'm sad you're here!"))
+
+
+
+
