69998862d13fc1cc89c1843b651efee19b8865d3
sukhada111 <sukhada9987@gmail.com>
2022-03-19 19:07:30 +0530

    chatbot model

---
+++ prediction_portal/chatbot/chat.py
@@ -0,0 +1,50 @@
+import random
+import json
+
+import torch
+
+from prediction_portal.chatbot.model import NeuralNet
+from prediction_portal.chatbot.nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+import os
+path=os.getcwd()+'\\prediction_portal\\chatbot\\intents.json'
+with open(path, 'r', encoding='utf-8') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Healthino"
+
+def get_response(msg):
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
+    else:
+        return "I do not understand..."
